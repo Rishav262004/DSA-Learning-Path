2eeeb4a8bc158eec22e21dcc66f0c96baa90d6a3
RISHAV MISHRA <136835572+Rishav262004@users.noreply.github.com>
2024-11-27 14:46:32 +0530

    Update Day_11.java
    2 questions on leetcode

--- Day 11/day_11_leetcode/Day_11.java
+++ Day 11/day_11_leetcode/Day_11.java
@@ -1,4 +1,5 @@
 /*
+
 https://leetcode.com/problems/first-missing-positive/description/
 
 First Missing Positive
@@ -25,6 +26,50 @@ class Solution {
 
         // If all positions are filled correctly, the missing number is n+1
         return n + 1;
+    }
+}
+
+------------------------------------------------------------------------------------------------------------------------------------------
+
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+
+Find First and Last Position of Element in Sorted Array
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+
+        int start = search(nums, target, true);
+        int end = search(nums, target, false);
+
+        ans[0] = start;
+        ans[1] = end;
+
+        return ans;
+    }
+
+    int search(int[] nums, int target, boolean findStartIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
     }
 }
 
