6e1135915f138852863b495dd1b1ac5e65660807
RISHAV MISHRA <136835572+Rishav262004@users.noreply.github.com>
2024-11-29 16:59:23 +0530

    Update Day_13.java

--- Day 13/gfg/Day_13.java
+++ Day 13/gfg/Day_13.java
@@ -62,17 +62,52 @@ class Solution {
 }
 
 
+----------------------------------------------- Problem 2 ---------------------------------------------------------
 
+// https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
 
+// Find position of an element in a sorted array of infinite numbers
 
+public class infinite_array_search {
+    public static void main(String args[]) {
+        int[] arr = { 1, 2, 5, 7, 6, 3, 8, 9 };
+        int target = 5;
 
+        int position = ans(arr, target);
+        System.out.println("Position of " + target + " is: " + position);
+    }
 
+    static int ans(int[] arr, int target) {
+        int start = 0;
+        int end = 1;
 
+        while (target > arr[end]) {
+            int temp = end + 1;  // this is my new start
 
+            end = (end - start + 1) * 2;
+            start = temp;
+        }
+        return binarySearch(arr, target, start, end);
+    }
 
+    static int binarySearch(int arr[], int target, int start, int end) {
 
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
+            if (arr[mid] == target) {
+                return mid;
+            }
 
+            if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
 
 
 
