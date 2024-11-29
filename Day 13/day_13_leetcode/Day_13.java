655e07dc8e17b401de82f3337adf22cebe4cf853
RISHAV MISHRA <136835572+Rishav262004@users.noreply.github.com>
2024-11-29 16:47:43 +0530

    Update Day_13.java

--- Day 13/day_13_leetcode/Day_13.java
+++ Day 13/day_13_leetcode/Day_13.java
@@ -42,4 +42,41 @@ class Solution {
     }
 }
 
+
+------------------------------------- Problem 2-----------------------------------------------------
+
+https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+
+
+You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+Return the index of the peak element.
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+
+        int start=0;
+        int end = arr.length -1;
+
+        while(start< end){
+            int mid = start + (end-start)/2;
+
+            if(arr[mid] >arr[mid+1]){
+                // you are in the decreasing part of the array;
+                //  this may be the ans but look at left;
+                // this is why end != mid-1;
+                end = mid;
+            }else{
+                //  you are in the increasing part of the array;
+                //  because we know that mid+1 element is greater than mid
+
+                start=mid+1;
+            }
+        }
+        // return start; or return end;
+        return start;
+    }
+}
+
+
 */
