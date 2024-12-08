6100f7ea7b850b62afcfa63c22bbf864bf6301a6
RISHAV MISHRA <136835572+Rishav262004@users.noreply.github.com>
2024-12-08 15:47:31 +0530

    Day_22.java

--- Day 22/day_22_leetcode/Day_22.java
+++ Day 22/day_22_leetcode/Day_22.java
@@ -1,5 +1,7 @@
 /*
 
+-------------------------------------------------- Problem 1 -----------------------------------------------------------
+
 https://leetcode.com/problems/divide-two-integers/description/
 
 Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
@@ -55,6 +57,105 @@ class Solution {
         }
 
         return isNegative ? -quotient : quotient;
+    }
+}
+
+
+--------------------------------------------- Problem 2 ----------------------------------------------------------------
+
+https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/1473393750/
+
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) 
+such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). 
+For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target 
+if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+
+---------------------------------------------------------- CODE --------------------------------------------------------
+
+class Solution {
+
+    public int search(int[] nums, int target) {
+
+        int pivot = findPivot(nums);
+
+        // if you did not find the pivot that means array is not rotated
+        if (pivot == -1) {
+            // just do normal binary search
+            return binarySearch(nums, target, 0, nums.length - 1);
+        }
+
+        // if pivot is found you have found two ascending arrays
+
+        // 3 cases
+        if (nums[pivot] == target) {
+            return pivot;
+        }
+
+        if (target >= nums[0]) {
+            return binarySearch(nums, target, 0, pivot - 1);
+        }
+
+        return binarySearch(nums, target, pivot + 1, nums.length - 1);
+    }
+
+    public int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public int findPivot(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            // 4 cases here
+            if (mid < end && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+
+            if (mid > start && nums[mid] < nums[mid - 1]) {
+                return mid - 1;
+            }
+
+            if (nums[mid] < nums[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
     }
 }
 
