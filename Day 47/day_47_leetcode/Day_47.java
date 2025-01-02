5f77826143b1f1b90c30e49126539153787e5fe4
RISHAV MISHRA <136835572+Rishav262004@users.noreply.github.com>
2025-01-02 06:15:54 +0530

    LC _POTD ADDED Day_47.java

--- Day 47/day_47_leetcode/Day_47.java
+++ Day 47/day_47_leetcode/Day_47.java
@@ -124,6 +124,75 @@ class Solution {
     }
 }
 
+----------------------------------------------------------------------------- POTD JAN 2 ---------------------------------------------------------------------------------
+
+https://leetcode.com/problems/count-vowel-strings-in-ranges/?envType=daily-question&envId=2025-01-02
+
+You are given a 0-indexed array of strings words and a 2D array of integers queries.
+
+Each query queries[i] = [li, ri] asks us to find the number of strings present in the range li to ri (both inclusive) of words that start and end with a vowel.
+
+Return an array ans of size queries.length, where ans[i] is the answer to the ith query.
+
+Note that the vowel letters are 'a', 'e', 'i', 'o', and 'u'.
+
+ 
+
+Example 1:
+
+Input: words = ["aba","bcb","ece","aa","e"], queries = [[0,2],[1,4],[1,1]]
+Output: [2,3,0]
+Explanation: The strings starting and ending with a vowel are "aba", "ece", "aa" and "e".
+The answer to the query [0,2] is 2 (strings "aba" and "ece").
+to query [1,4] is 3 (strings "ece", "aa", "e").
+to query [1,1] is 0.
+We return [2,3,0].
+Example 2:
+
+Input: words = ["a","e","i"], queries = [[0,2],[0,1],[2,2]]
+Output: [3,2,1]
+Explanation: Every string satisfies the conditions, so we return [3,2,1].
+
+----------------------------------------------------------------------------------- CODE ------------------------------------------------------------------------------------
+
+class Solution {
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        int n = words.length;
+        int m = queries.length;
+        
+        // Define the vowels
+        String vowels = "aeiou";
+        
+        // Step 1: Preprocess the words to find which strings start and end with a vowel
+        int[] isValid = new int[n];
+        for (int i = 0; i < n; i++) {
+            String word = words[i];
+            if (vowels.indexOf(word.charAt(0)) != -1 && vowels.indexOf(word.charAt(word.length() - 1)) != -1) {
+                isValid[i] = 1; // This word is valid
+            } else {
+                isValid[i] = 0; // This word is not valid
+            }
+        }
+        
+        // Step 2: Create the prefix sum array
+        int[] prefix = new int[n + 1]; // prefix[i] will store the count of valid words from 0 to i-1
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + isValid[i - 1];
+        }
+        
+        // Step 3: Answer the queries
+        int[] ans = new int[m];
+        for (int i = 0; i < m; i++) {
+            int li = queries[i][0];
+            int ri = queries[i][1];
+            ans[i] = prefix[ri + 1] - prefix[li];
+        }
+        
+        return ans;
+    }
+}
+
+
 
 
 */
